635b750127545ee1b7ec7251be95898102a68495
Haile <hailecst@gmail.com>
2026-02-17 19:49:41 +0300

    feat: repo and model created for trip-service

---
+++ services/trip-service/internal/infrastructure/repository/inmem.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/high-la/ride-sharing/services/trip-service/internal/domain"
+)
+
+type inmemRepository struct {
+	trips     map[string]*domain.TripModel
+	rideFares map[string]*domain.RideFareModel
+}
+
+func NewInmemRepositiry() *inmemRepository {
+
+	return &inmemRepository{
+		trips:     make(map[string]*domain.TripModel),     // create empty map
+		rideFares: make(map[string]*domain.RideFareModel), // create empty map
+	}
+}
+
+func (r *inmemRepository) CreateTrip(ctx context.Context, trip *domain.TripModel) (*domain.TripModel, error) {
+
+	r.trips[trip.ID.Hex()] = trip //.Hex is a trick to change mongo object type to string
+
+	return trip, nil
+}
